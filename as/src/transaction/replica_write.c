13787f675c87fe6376c7a736fb6190722d4c6e52
Andrew Gooding <andy@aerospike.com>
2019-10-17 23:18:31 -0700

    Added compatibility version check around sending RW_INFO_XDR bit during replication.

--- as/src/transaction/replica_write.c
+++ as/src/transaction/replica_write.c
@@ -564,7 +564,8 @@ pack_info_bits(as_transaction* tr)
 {
 	uint32_t info = 0;
 
-	if (as_transaction_is_xdr(tr)) {
+	// TODO - old pickle - remove in "six months".
+	if (as_exchange_min_compatibility_id() < 3 && as_transaction_is_xdr(tr)) {
 		info |= RW_INFO_XDR;
 	}
 
