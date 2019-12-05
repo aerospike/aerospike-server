66fed6251049309c6780aeb1baa62b15c5690a02
Andrew Gooding <andy@aerospike.com>
2019-12-05 11:09:38 -0800

    Comment tweak (build trigger).

--- as/include/fabric/partition.h
+++ as/include/fabric/partition.h
@@ -1,7 +1,7 @@
 /*
  * partition.h
  *
- * Copyright (C) 2016 Aerospike, Inc.
+ * Copyright (C) 2016-2019 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
