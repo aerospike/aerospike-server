a2b3258c85f097d8da7dacd2b4536cf3aa3fad52
Andrew Gooding <andy@aerospike.com>
2019-12-12 17:13:07 -0800

    Comment tweak (trigger build).

--- as/src/base/aggr.c
+++ as/src/base/aggr.c
@@ -1,7 +1,7 @@
 /*
  * aggr.c
  *
- * Copyright (C) 2014-2015 Aerospike, Inc.
+ * Copyright (C) 2014-2019 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
