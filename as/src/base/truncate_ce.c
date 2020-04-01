972eba6484d076af3ec3318e7f832e75ad6e2a46
Andrew Gooding <andy@aerospike.com>
2020-03-31 23:16:03 -0700

    Comment tweak (trigger build).

--- as/src/base/truncate_ce.c
+++ as/src/base/truncate_ce.c
@@ -1,7 +1,7 @@
 /*
  * truncate_ce.c
  *
- * Copyright (C) 2017-2019 Aerospike, Inc.
+ * Copyright (C) 2017-2020 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
