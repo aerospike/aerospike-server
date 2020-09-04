ad61df47934352e77089cb25f114a726f8d735ee
Andrew Gooding <andy@aerospike.com>
2020-09-04 13:14:33 -0700

    AER-6287 - when writing over a tombstone with TTL_DONT_UPDATE, use namespace default-ttl. Refactored create-only policy check.

--- as/src/transaction/rw_utils_ce.c
+++ as/src/transaction/rw_utils_ce.c
@@ -149,14 +149,6 @@ transition_delete_metadata(as_transaction* tr, as_record* r, bool is_delete)
 // Private API - for enterprise separation only.
 //
 
-bool
-create_only_check(const as_record* r, const as_msg* m)
-{
-	// Ok (return true) if no requirement.
-	return (m->info2 & AS_MSG_INFO2_CREATE_ONLY) == 0;
-}
-
-
 void
 write_delete_record(as_record* r, as_index_tree* tree)
 {
