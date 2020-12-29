be59c159a00533711ca13aa22c8dfb339b2cdc0b
Andrew Gooding <andy@aerospike.com>
2020-12-29 09:18:21 -0800

    Comment tweak (trigger build).

--- cf/include/linear_hist.h
+++ cf/include/linear_hist.h
@@ -1,7 +1,7 @@
 /*
  * linear_hist.h
  *
- * Copyright (C) 2016 Aerospike, Inc.
+ * Copyright (C) 2016-2020 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
