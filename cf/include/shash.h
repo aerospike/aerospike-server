8abe414d54c4d9cae80992c033fa802d579321b1
Andrew Gooding <andy@aerospike.com>
2021-04-05 14:05:42 -0700

    Comment tweak (trigger build).

--- cf/include/shash.h
+++ cf/include/shash.h
@@ -1,7 +1,7 @@
 /*
  * shash.h
  *
- * Copyright (C) 2017 Aerospike, Inc.
+ * Copyright (C) 2017-2021 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
