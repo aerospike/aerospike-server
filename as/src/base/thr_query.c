e78daa78c87e4a8294fff1fe886ebb9a90eade1a
Andrew Gooding <andy@aerospike.com>
2021-05-26 10:38:29 -0700

    AER-6438 - ensure background ops query is done before detaching it from monitor.

--- as/src/base/thr_query.c
+++ as/src/base/thr_query.c
@@ -2513,7 +2513,8 @@ query_qtr_check_and_requeue(as_query_transaction *qtr)
 	if (qtr_finished(qtr)) {
 		if ((cf_atomic32_get(qtr->n_qwork_active) == 0)
 				&& (cf_atomic32_get(qtr->n_io_outstanding) == 0)
-				&& (cf_atomic32_get(qtr->n_udf_tr_queued) == 0)) {
+				&& (cf_atomic32_get(qtr->n_udf_tr_queued) == 0)
+				&& (cf_atomic32_get(qtr->n_ops_tr_queued) == 0)) {
 			cf_detail(AS_QUERY, "Request is finished");
 			return AS_QUERY_DONE;
 		}
@@ -2548,7 +2549,8 @@ query_qtr_check_and_requeue(as_query_transaction *qtr)
 		qtr_lock(qtr);
 		if ((cf_atomic32_get(qtr->n_qwork_active) != 0)
 				|| (cf_atomic32_get(qtr->n_io_outstanding) != 0)
-				|| (cf_atomic32_get(qtr->n_udf_tr_queued) != 0)) {
+				|| (cf_atomic32_get(qtr->n_udf_tr_queued) != 0)
+				|| (cf_atomic32_get(qtr->n_ops_tr_queued) != 0)) {
 			cf_detail(AS_QUERY, "Job Setup for Requeue %p", qtr);
 
 			// Release of one of the above will perform requeue... look for
