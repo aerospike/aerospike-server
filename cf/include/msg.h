e96c653224ed479347bb04be82ca9fe2e34a712b
Andrew Gooding <andy@aerospike.com>
2021-07-30 13:25:41 -0700

    Comment tweak (trigger build).

--- cf/include/msg.h
+++ cf/include/msg.h
@@ -1,7 +1,7 @@
 /*
  * msg.h
  *
- * Copyright (C) 2008-2020 Aerospike, Inc.
+ * Copyright (C) 2008-2021 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
