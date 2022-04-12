5ec035c91e43f7d31f1014680530fd4206a2cf91
Andrew Gooding <andy@aerospike.com>
2022-04-12 09:01:33 -0700

    AER-6526 - throttle truncate and nsup if they threaten to over-fill the sindex gc rlist.

--- as/src/sindex/gc.c
+++ as/src/sindex/gc.c
@@ -67,6 +67,8 @@ typedef struct gc_offset_s {
 #define CREATE_LIST_PER_ITERATION_LIMIT 10000
 #define PROCESS_LIST_PER_ITERATION_LIMIT  10
 
+#define THROTTLE_THRESHOLD (64 * 1024 * 1024)
+
 
 //==========================================================
 // Forward declarations.
@@ -129,8 +131,33 @@ as_sindex_gc_record(as_namespace* ns, as_index_ref* r_ref)
 	cf_mutex_lock(&ns->si_gc_list_mutex);
 
 	push_to_rlist(ns, r_ref);
+	ns->si_gc_rlist_full = cf_queue_sz(ns->si_gc_rlist) >= THROTTLE_THRESHOLD;
 
 	cf_mutex_unlock(&ns->si_gc_list_mutex);
+}
+
+void
+as_sindex_gc_record_throttle(as_namespace* ns)
+{
+	if (! ns->si_gc_rlist_full) {
+		return;
+	}
+
+	while (true) {
+		cf_ticker_info(AS_SINDEX, "{%s} gc rlist full - throttling", ns->name);
+
+		sleep(1);
+
+		cf_mutex_lock(&ns->si_gc_list_mutex);
+
+		if (cf_queue_sz(ns->si_gc_rlist) < THROTTLE_THRESHOLD) {
+			ns->si_gc_rlist_full = false; // optional - but can't hurt
+			cf_mutex_unlock(&ns->si_gc_list_mutex);
+			return;
+		}
+
+		cf_mutex_unlock(&ns->si_gc_list_mutex);
+	}
 }
 
 void
