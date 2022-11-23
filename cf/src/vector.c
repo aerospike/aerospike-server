f35df2ccecada700290c9ecc1ab7ee8090045a6f
Andrew Gooding <andy@aerospike.com>
2022-11-23 12:23:48 -0800

    Comment tweak (trigger build).

--- cf/src/vector.c
+++ cf/src/vector.c
@@ -1,7 +1,7 @@
 /*
  * vector.c
  *
- * Copyright (C) 2008-2020 Aerospike, Inc.
+ * Copyright (C) 2008-2022 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
