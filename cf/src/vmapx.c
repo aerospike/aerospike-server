8dc949d032aa3b81437983fb8c773f7c53b59b04
Andrew Gooding <andy@aerospike.com>
2023-03-14 12:34:03 -0700

    Comment tweak (trigger build).

--- cf/src/vmapx.c
+++ cf/src/vmapx.c
@@ -1,7 +1,7 @@
 /*
  * vmapx.c
  *
- * Copyright (C) 2012-2020 Aerospike, Inc.
+ * Copyright (C) 2012-2023 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
