e5e6745b925cf1df5b1ccb8346183676a443c841
Andrew Gooding <andy@aerospike.com>
2023-05-31 08:09:25 -0700

    Comment tweak (trigger build).

--- as/src/base/namespace_ce.c
+++ as/src/base/namespace_ce.c
@@ -1,7 +1,7 @@
 /*
  * namespace_ce.c
  *
- * Copyright (C) 2014-2020 Aerospike, Inc.
+ * Copyright (C) 2014-2023 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
