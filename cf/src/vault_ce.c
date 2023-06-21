ddcf14f335d8e659468c78782a5926752edcf4a9
Andrew Gooding <andy@aerospike.com>
2023-06-21 16:49:31 -0700

    Comment tweak (trigger build).

--- cf/src/vault_ce.c
+++ cf/src/vault_ce.c
@@ -1,7 +1,7 @@
 /*
  * vault_ce.c
  *
- * Copyright (C) 2020 Aerospike, Inc.
+ * Copyright (C) 2020-2023 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
