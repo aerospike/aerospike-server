c9012e891b4c0b1c06283ac45e4bf6369d7eedbf
Kevin Porter <kporter@aerospike.com>
2023-09-05 16:49:59 -0700

    Add EE deps - add a dry-run mode - remove script dependencies.

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -3,60 +3,42 @@ set -Eeuo pipefail
 
 function usage() {
 	echo
-	echo "Usage: $0 [-r | --runtime-only]" 1>&2
+	echo "Usage: $0 [-d -r -h]"
 	echo
-	echo "-r|--runtime-only: install the packages for runtime only (skip the dev packages)"
-	echo
+	echo "-d: print the package update commands instead of running them (dry-run)"
+	echo "-r: install the packages for runtime only (skip the dev packages)"
+	echo "-h: print this help message"
 }
 
 
-function install_script_dependencies() {
-	# Needed for 'which' and 'getopt'.
+function log_info() {
+	echo -e "LOG-INFO\t\e[32m$1\e[0m" 1>&2
+}
 
-	echo -e "\e[32mInstalling script dependencies, os=${OS}...\e[0m"
 
-	case "$OS" in
-	'debian10' | 'debian11' | 'debian12' | 'ubuntu20.04' | 'ubuntu22.04' )
-		apt-get install -y --no-install-recommends util-linux debianutils
-		;;
-	# centos7 is the base image for rhel7
-	'amzn2023' | 'centos7' | 'rhel8' | 'rhel9' )
-		yum install -y util-linux which
-		;;
-	*)
-		echo -e "\e[31mAerospike server (CE) currently does not support the distribution ${OS}.\e[0m"
-		exit 1
-	esac
+function log_warn() {
+	echo -e "LOG-WARN\t\e[32m$1\e[0m" 1>&2
 }
 
 
 function parse_args() {
 	RUNTIME_ONLY="false"
+	DRY_RUN="false"
 
-	local parsed_args=$(getopt -a -n test -o rh --long runtime-only,help -- "$@")
-	local valid_args=$?
-
-	if [ "${valid_args}" != "0" ]; then
-		usage
-	fi
-
-	eval set -- "${parsed_args}"
-	while true; do
-		case "$1" in
-		-r | --runtime-only)
-			RUNTIME_ONLY="true"
-			shift
+	while getopts "dhr" opt; do
+		case "${opt}" in
+		d )
+			DRY_RUN="true"
 			;;
-		-h | --help)
+		h )
 			usage
 			exit 0
 			;;
-		--)
-			shift
-			break
+		r )
+			RUNTIME_ONLY="true"
 			;;
 		*)
-			echo -e "\e[31mUnexpected option: $1\e[0m"
+			log_info "Unexpected option: ${opt}"
 			usage
 			exit 1
 			;;
@@ -65,24 +47,33 @@ function parse_args() {
 }
 
 
+function maybe() {
+	if [ "$DRY_RUN" = "false" ]; then
+		$1;
+	else
+	    echo "$1";
+	fi
+}
+
+
 function main() {
 	OS=$(./build/os_version -long)
-	install_script_dependencies
 	parse_args "$@"
 
 	SUDO=""
-	if which sudo >/dev/null; then
-		SUDO="$(which sudo)"
+	if command -v sudo >/dev/null; then
+		SUDO="$(command -v sudo)"
 	fi
 
-	echo -e "\e[32mInstalling server dependencies, os=${OS}, runtime-only=${RUNTIME_ONLY}...\e[0m"
+	log_info "Installing server dependencies, os=${OS}, runtime-only=${RUNTIME_ONLY}..."
 
 	case "$OS" in
-	'debian10' | 'debian11' | 'debian12' | 'ubuntu20.04' | 'ubuntu22.04' )
-		${SUDO} apt-get update
-		packages=(libssl-dev zlib1g-dev)  # # Common packages (build + Runtime)
+	'debian11' | 'debian12' | 'ubuntu20.04' | 'ubuntu22.04' )
+		maybe "${SUDO} apt-get update"
+		packages=(libssl-dev zlib1g-dev)  # Common packages (build + Runtime)
 		# Add packages for build-only mode (i.e., runtime-only is not set)
 		if [ "$RUNTIME_ONLY" = "false" ]; then
+			# CE dependencies.
 			packages+=(autoconf \
 				automake \
 				cmake \
@@ -91,18 +82,22 @@ function main() {
 				g++ \
 				git \
 				libtool \
-				make)
+				make \
+				pkg-config)
+			# EE dependencies.
+			packages+=(libcurl4-openssl-dev
+				   libldap2-dev)
 		fi
 
-		DEBIAN_FRONTEND=noninteractive ${SUDO} apt-get install -y --no-install-recommends "${packages[@]}"
+		maybe "DEBIAN_FRONTEND=noninteractive ${SUDO} apt-get install -y --no-install-recommends \"${packages[*]}\""
 		;;
 
-	# centos7 is the base image for rhel7
-	'amzn2023' | 'centos7' | 'rhel8' | 'rhel9' )
+	'amzn2023' | 'rhel8' | 'rhel9' )
 		packages=(openssl-devel zlib-devel)  # Common packages (build + Runtime).
 
 		# Add packages for build-only mode (i.e., runtime-only is not set)
 		if [ "$RUNTIME_ONLY" = "false" ]; then
+			# CE dependencies.
 			packages+=(autoconf \
 				automake \
 				cmake \
@@ -111,40 +106,19 @@ function main() {
 				libtool \
 				make \
 				rpm-build)
+			# EE dependencies.
+			packages+=(libcurl-devel
+				   openldap-devel)
 		fi
 
-		${SUDO} yum install -y "${packages[@]}"
-
-		# Special provision for centos7.
-		if [ "$RUNTIME_ONLY" = "false" ] && [ "$OS" = "centos7" ]; then
-			# Should install the release repo's before installing
-			# the packages.
-			${SUDO} yum install -y centos-release-scl  # software collection repo
-			${SUDO} yum install -y epel-release
-
-			${SUDO} yum install -y \
-				cmake3 \
-				devtoolset-9-gcc \
-				devtoolset-9-gcc-c++
-		fi
+		maybe "${SUDO} yum install -y \"${packages[*]}\""
 		;;
 	*)
-		echo -e "\e[31mAerospike server (CE) currently does not support the distribution ${OS}.\e[0m"
+		log_warn "Aerospike server (CE) currently does not support the distribution ${OS}."
 		;;
 	esac
 
-	if [ -n  "${EEREPO-}" ]; then
-		opt_flags=()
-		[[ ${RUNTIME_ONLY} == true ]] && opt_flags+=(--runtime-only)
-
-		# 'set +u' as a  workaround for a centos7 bug (unbound variable);
-		# "${opt_flags[@]:+${opt_flags[@]}}" could be another option.
-		[[ $OS = "centos7" ]] && set +u
-		"${EEREPO}"/bin/install-ee-dependencies.sh --os="${OS}" "${opt_flags[@]}"
-		[[ $OS = "centos7" ]] && set -u
-	fi
-
-	echo -e "\e[32mFinished.\e[0m"
+	log_info "Finished."
 }
 
 main "$@"
