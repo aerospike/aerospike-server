608fbde93d76515259b47c7e2994cb266a40a8e0
Andrew Gooding <andy@aerospike.com>
2024-02-15 13:41:31 -0800

    Comment tweak (trigger build).

--- as/include/storage/storage.h
+++ as/include/storage/storage.h
@@ -1,7 +1,7 @@
 /*
  * storage.h
  *
- * Copyright (C) 2009-2023 Aerospike, Inc.
+ * Copyright (C) 2009-2024 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
