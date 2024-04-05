52372db101b2c3a8e48c37760ef410379e18c1b1
Andrew Gooding <andy@aerospike.com>
2024-04-05 09:40:58 -0700

    TLS code convention cleanup.
    Co-authored-by: Daudkhan Pathan <dpathan@aerospike.com>

--- cf/include/tls.h
+++ cf/include/tls.h
@@ -1,7 +1,7 @@
 /*
  * tls.h
  *
- * Copyright (C) 2016-2023 Aerospike, Inc.
+ * Copyright (C) 2016-2024 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
@@ -22,65 +22,83 @@
 
 #pragma once
 
+//==========================================================
+// Includes.
+//
+
 #include <stdint.h>
 
 #include "socket.h"
 
-#define MAX_TLS_SPECS 10
+
+//==========================================================
+// Forward declarations.
+//
 
 typedef struct cf_tls_info_s cf_tls_info;
 
+
+//==========================================================
+// Typedefs & constants.
+//
+
+#define MAX_TLS_SPECS 10
+
 typedef struct cf_tls_spec_s {
-	char *ca_file;
-	char *ca_path;
-	char *cert_blacklist;
-	char *cert_file;
-	char *cipher_suite;
-	char *key_file;
-	char *key_file_password;
-	char *name;
-	char *protocols;
+	char* ca_file;
+	char* ca_path;
+	char* cert_blacklist;
+	char* cert_file;
+	char* cipher_suite;
+	char* key_file;
+	char* key_file_password;
+	char* name;
+	char* protocols;
 } cf_tls_spec;
+
+
+//==========================================================
+// Public API.
+//
 
 void cf_tls_init(void);
 void cf_tls_start(void);
 char* cf_resolve_tls_name(char* tls_name, const char* cluster_name, const char* which);
 
-void tls_cleanup();
-void tls_thread_cleanup();
+void tls_cleanup(void);
+void tls_thread_cleanup(void);
 
-void tls_socket_init(cf_socket *sock);
-void tls_socket_term(cf_socket *sock);
-int tls_socket_shutdown(cf_socket *sock);
-void tls_socket_close(cf_socket *sock);
+void tls_socket_init(cf_socket* sock);
+void tls_socket_term(cf_socket* sock);
+int tls_socket_shutdown(cf_socket* sock);
+void tls_socket_close(cf_socket* sock);
 
-char *tls_read_password(const char *path);
-cf_tls_info *tls_config_server_context(cf_tls_spec *tspec, bool auth_client, uint32_t n_peer_names, char **peer_names);
-cf_tls_info *tls_config_intra_context(cf_tls_spec *tspec, const char *which);
+char* tls_read_password(const char* path);
+cf_tls_info* tls_config_server_context(cf_tls_spec* tspec, bool auth_client, uint32_t n_peer_names, char** peer_names);
+cf_tls_info* tls_config_intra_context(cf_tls_spec* tspec, const char* which);
 
 void tls_socket_prepare_server(cf_socket* sock, cf_tls_info* info);
 void tls_socket_prepare_client(cf_socket* sock, cf_tls_info* info);
 
-static inline bool tls_socket_needs_handshake(cf_socket *sock)
+void tls_socket_must_not_have_data(cf_socket* sock, const char* caller);
+
+int tls_socket_accept(cf_socket* sock);
+int tls_socket_connect(cf_socket* sock);
+int tls_socket_accept_block(cf_socket* sock, uint32_t timeout);
+int tls_socket_connect_block(cf_socket* sock, uint32_t timeout);
+
+int tls_socket_recv(cf_socket* sock, void* buf, size_t sz, int32_t flags, uint64_t timeout_msec);
+
+int tls_socket_send(cf_socket* sock, void const* buf, size_t sz, int32_t flags, uint64_t timeout_msec);
+
+int tls_socket_pending(cf_socket* sock);
+
+void tls_init_change_check(cf_tls_spec* tspec);
+uint32_t tls_get_refresh_period(void);
+void tls_set_refresh_period(uint32_t period);
+
+static inline bool
+tls_socket_needs_handshake(cf_socket* sock)
 {
 	return sock->state == CF_SOCKET_STATE_TLS_HANDSHAKE;
 }
-
-void tls_socket_must_not_have_data(cf_socket *sock, const char *caller);
-
-int tls_socket_accept(cf_socket *sock);
-int tls_socket_connect(cf_socket *sock);
-int tls_socket_accept_block(cf_socket *sock, uint32_t timeout);
-int tls_socket_connect_block(cf_socket *sock, uint32_t timeout);
-
-int tls_socket_recv(cf_socket *sock, void *buf, size_t sz, int32_t flags,
-					uint64_t timeout_msec);
-
-int tls_socket_send(cf_socket *sock, void const *buf, size_t sz, int32_t flags,
-					uint64_t timeout_msec);
-
-int tls_socket_pending(cf_socket *sock);
-
-void tls_init_change_check(cf_tls_spec* tspec);
-uint32_t tls_get_refresh_period();
-void tls_set_refresh_period(uint32_t period);
--- cf/src/tls_ce.c
+++ cf/src/tls_ce.c
@@ -1,7 +1,7 @@
 /*
  * tls.c
  *
- * Copyright (C) 2016-2020 Aerospike, Inc.
+ * Copyright (C) 2016-2024 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
@@ -19,11 +19,20 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/
  */
+
+//==========================================================
+// Includes.
+//
+
 #include <openssl/ssl.h>
 
 #include "log.h"
 #include "socket.h"
 #include "tls.h"
+
+
+//==========================================================
+// Public API
 
 void
 cf_tls_init(void)
@@ -42,61 +51,64 @@ cf_resolve_tls_name(char* tls_name, const char* cluster_name, const char* which)
 }
 
 void
-tls_cleanup()
+tls_cleanup(void)
 {
 }
 
 void
-tls_thread_cleanup()
+tls_thread_cleanup(void)
 {
 }
 
 void
-tls_socket_init(cf_socket *sock)
+tls_socket_init(cf_socket* sock)
 {
 	sock->ssl = NULL;
 }
 
 void
-tls_socket_term(cf_socket *sock)
+tls_socket_term(cf_socket* sock)
 {
-	if (sock->ssl) {
+	if (sock->ssl != NULL) {
 		cf_crash(CF_TLS, "unexpected TLS state");
 	}
 }
 
 int
-tls_socket_shutdown(cf_socket *sock)
+tls_socket_shutdown(cf_socket* sock)
 {
-	if (sock->ssl) {
+	if (sock->ssl != NULL) {
 		cf_crash(CF_TLS, "unexpected TLS state");
 	}
+
 	return -1;
 }
 
 void
-tls_socket_close(cf_socket *sock)
+tls_socket_close(cf_socket* sock)
 {
-	if (sock->ssl) {
+	if (sock->ssl != NULL) {
 		cf_crash(CF_TLS, "unexpected TLS state");
 	}
 }
 
-char *tls_read_password(const char *path)
+char*
+tls_read_password(const char* path)
 {
 	cf_crash(CF_TLS, "unexpected TLS state");
 	return NULL;
 }
 
-cf_tls_info *
-tls_config_server_context(cf_tls_spec *tspec, bool auth_client, uint32_t n_peer_names, char **peer_names)
+cf_tls_info*
+tls_config_server_context(cf_tls_spec* tspec, bool auth_client,
+		uint32_t n_peer_names, char** peer_names)
 {
 	cf_crash(CF_TLS, "unexpected TLS state");
 	return NULL;
 }
 
-cf_tls_info *
-tls_config_intra_context(cf_tls_spec *tspec, const char *which)
+cf_tls_info*
+tls_config_intra_context(cf_tls_spec* tspec, const char* which)
 {
 	cf_crash(CF_TLS, "unexpected TLS state");
 	return NULL;
@@ -115,7 +127,7 @@ tls_socket_prepare_client(cf_socket* sock, cf_tls_info* info)
 }
 
 void
-tls_socket_must_not_have_data(cf_socket *sock, const char *caller)
+tls_socket_must_not_have_data(cf_socket* sock, const char* caller)
 {
 	if (sock->state == CF_SOCKET_STATE_NON_TLS) {
 		return;
@@ -125,35 +137,35 @@ tls_socket_must_not_have_data(cf_socket *sock, const char *caller)
 }
 
 int
-tls_socket_accept(cf_socket *sock)
+tls_socket_accept(cf_socket* sock)
 {
 	cf_crash(CF_TLS, "unexpected TLS state");
 	return 1;
 }
 
 int
-tls_socket_connect(cf_socket *sock)
+tls_socket_connect(cf_socket* sock)
 {
 	cf_crash(CF_TLS, "unexpected TLS state");
 	return 1;
 }
 
 int
-tls_socket_accept_block(cf_socket *sock, uint32_t timeout)
+tls_socket_accept_block(cf_socket* sock, uint32_t timeout)
 {
 	cf_crash(CF_TLS, "unexpected TLS state");
 	return 1;
 }
 
 int
-tls_socket_connect_block(cf_socket *sock, uint32_t timeout)
+tls_socket_connect_block(cf_socket* sock, uint32_t timeout)
 {
 	cf_crash(CF_TLS, "unexpected TLS state");
 	return 1;
 }
 
 int
-tls_socket_recv(cf_socket *sock, void *buf, size_t sz, int32_t flags,
+tls_socket_recv(cf_socket* sock, void* buf, size_t sz, int32_t flags,
 				uint64_t deadline_msec)
 {
 	cf_crash(CF_TLS, "unexpected TLS state");
@@ -161,7 +173,7 @@ tls_socket_recv(cf_socket *sock, void *buf, size_t sz, int32_t flags,
 }
 
 int
-tls_socket_send(cf_socket *sock, void const *buf, size_t sz, int32_t flags,
+tls_socket_send(cf_socket* sock, void const* buf, size_t sz, int32_t flags,
 				uint64_t deadline_msec)
 {
 	cf_crash(CF_TLS, "unexpected TLS state");
@@ -169,7 +181,7 @@ tls_socket_send(cf_socket *sock, void const *buf, size_t sz, int32_t flags,
 }
 
 int
-tls_socket_pending(cf_socket *sock)
+tls_socket_pending(cf_socket* sock)
 {
 	return 0;
 }
@@ -180,7 +192,7 @@ tls_init_change_check(cf_tls_spec* tspec)
 }
 
 uint32_t
-tls_get_refresh_period()
+tls_get_refresh_period(void)
 {
 	return 0;
 }
