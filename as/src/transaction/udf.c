93f46256b464ead12111f6e9c07658b90bae0711
Andrew Gooding <andy@aerospike.com>
2024-08-02 13:50:18 -0700

    AER-6758 - changed how we flag records as "loaded" in UDFs, so we can detect when the bins array is invalidated by utilities like record_matches_query().

--- as/src/transaction/udf.c
+++ as/src/transaction/udf.c
@@ -812,6 +812,7 @@ udf_master_apply(udf_call* call, rw_request* rw)
 
 	urecord.r_ref = &r_ref;
 	urecord.rd = &rd;
+	urecord_set_not_loaded(&urecord);
 
 	if (get_rv == 0) {
 		uint8_t open_rv = open_existing_record(&urecord);
@@ -1193,7 +1194,7 @@ udf_master_failed(udf_record* urecord, as_rec* urec, as_result* result,
 	if (urecord->is_open) {
 		as_index_ref* r_ref = urecord->r_ref;
 
-		if (urecord->is_loaded) {
+		if (urecord_is_loaded(urecord)) {
 			as_storage_rd* rd = urecord->rd;
 
 			if (urecord->has_updates && ns->storage_data_in_memory) {
@@ -1257,7 +1258,7 @@ udf_master_done(udf_record* urecord, as_rec* urec, as_result* result,
 	as_namespace* ns = tr->rsv.ns;
 
 	if (urecord->is_open) {
-		if (urecord->is_loaded) {
+		if (urecord_is_loaded(urecord)) {
 			as_storage_record_close(urecord->rd);
 		}
 
