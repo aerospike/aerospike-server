260e4dc5f706b2de682db1558f1977d93b6e98e6
Andrew Gooding <andy@aerospike.com>
2024-09-18 10:44:31 -0700

    Comment tweak (trigger build).

--- as/include/base/xdr.h
+++ as/include/base/xdr.h
@@ -1,7 +1,7 @@
 /*
  * xdr.h
  *
- * Copyright (C) 2020 Aerospike, Inc.
+ * Copyright (C) 2020-2024 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
