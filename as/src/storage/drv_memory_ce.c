1c3b94d11edbdea7da84dca50c945ba3a9c5fcc7
Andrew Gooding <andy@aerospike.com>
2024-09-30 09:39:42 -0700

    Comment tweak (trigger build).

--- as/src/storage/drv_memory_ce.c
+++ as/src/storage/drv_memory_ce.c
@@ -1,7 +1,7 @@
 /*
  * drv_memory_ce.c
  *
- * Copyright (C) 2016-2020 Aerospike, Inc.
+ * Copyright (C) 2016-2024 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
