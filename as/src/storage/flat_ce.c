c048b96a378b0ba5b921588a9ac0382de8aed1d5
Andrew Gooding <andy@aerospike.com>
2024-10-15 17:10:38 -0700

    Comment tweak (trigger build).

--- as/src/storage/flat_ce.c
+++ as/src/storage/flat_ce.c
@@ -1,7 +1,7 @@
 /*
  * flat_ce.c
  *
- * Copyright (C) 2019-2020 Aerospike, Inc.
+ * Copyright (C) 2019-2024 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
