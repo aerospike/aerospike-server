107560b58f618a4437e118b6c6611a7d5fb9c74c
Andrew Gooding <andy@aerospike.com>
2025-01-15 15:24:24 -0800

    AER-6791 - allow set filters on aggregations and background queries (like on basic queries) when the query uses a secondary index on the whole namespace.

--- as/src/query/query.c
+++ as/src/query/query.c
@@ -2332,7 +2332,7 @@ aggr_query_job_reduce_cb(as_index_ref* r_ref, int64_t bval, void* udata)
 
 	as_index* r = r_ref->r;
 
-	if (_job->si == NULL && excluded_set(r, _job->set_id)) {
+	if (excluded_set(r, _job->set_id)) {
 		as_record_done(r_ref, ns);
 		return true;
 	}
@@ -2732,7 +2732,7 @@ udf_bg_query_job_reduce_cb(as_index_ref* r_ref, int64_t bval, void* udata)
 
 	as_index* r = r_ref->r;
 
-	if (_job->si == NULL && excluded_set(r, _job->set_id)) {
+	if (excluded_set(r, _job->set_id)) {
 		as_record_done(r_ref, ns);
 		return true;
 	}
@@ -3133,7 +3133,7 @@ ops_bg_query_job_reduce_cb(as_index_ref* r_ref, int64_t bval, void* udata)
 
 	as_index* r = r_ref->r;
 
-	if (_job->si == NULL && excluded_set(r, _job->set_id)) {
+	if (excluded_set(r, _job->set_id)) {
 		as_record_done(r_ref, ns);
 		return true;
 	}
