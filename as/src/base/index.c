9ae80c7bf1de150fd5b9195466e6d45381c2d7e6
Sri Varun Poluri <varun@aerospike.com>
2026-01-08 11:47:51 +0530

    AER-6873 - Build trigger.

--- as/src/base/index.c
+++ as/src/base/index.c
@@ -1,7 +1,7 @@
 /*
  * index.c
  *
- * Copyright (C) 2012-2021 Aerospike, Inc.
+ * Copyright (C) 2012-2026 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
